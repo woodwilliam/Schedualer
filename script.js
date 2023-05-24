4f71d36bf16e5c0be2ee8a93632790f34554d161
Dr. Taintlove <sirwilliamwood@gmail.com>
2023-05-24 11:35:30 -0500

    installed dayjs, grabbed save bttn, edited some html

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var saveBtn = $('#save');
+const dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
+
+let dateRightNow = dayjs();
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,7 +14,12 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  saveBtn.on('click', function(){
+    timeStamp = ``
+    textContent = ``
+  })
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
