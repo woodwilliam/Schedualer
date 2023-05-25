f41a3a61c301dd3464bab6ad07ebac5fbdc62802
Dr. Taintlove <sirwilliamwood@gmail.com>
2023-05-24 22:16:26 -0500

    figured out Jquery! got the stuff to work

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var saveBtn = $('#save');
-var textBody = $('#description')
+var saveBtn = $("#save");
 // getting the date and time for the moment the page is rendered
-let dateRightNow = dayjs().format('dddd, MMMM DD, YYYY');
-let timeRightNow = dayjs().format('h:mm a');
-let hourValue = dayjs().format('h');
-console.log(dateRightNow);
-console.log(timeRightNow);
-console.log(hourValue);
-$('#nowDay').text(dateRightNow)
-$('#nowTime').text(timeRightNow)
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  saveBtn.on('click', function(){
-    timeStamp = ``
-    textContent = ``
-  })
+let dateRightNow = dayjs().format("dddd, MMMM DD, YYYY");
+let timeRightNow = dayjs().format("h:mm a");
+let hourValue = dayjs().hour();
+$("#nowDay").text(dateRightNow);
+$("#nowTime").text(timeRightNow);
 
+$(document).ready(function () {
+    $("#theGreatDeleter").on("click", function (event) {
+      event.preventDefault;
+      $("textarea").val("");
+      localStorage.clear();
+    });
+    saveBtn.on("click", function () {
+      let = textiscle = $(this).siblings(".description").val();
+      let = postTime = $(this).parent().attr("id");
+      localStorage.setItem(postTime, textiscle);
+    });
+      
+function timeyWimey() {
+  $(".time-block").each(function () {
+    let hrVal = parseInt($(this).attr("id").split("-")[0]);
+    console.log(hourValue);
+    console.log(hrVal);
+    if (hrVal > hourValue) {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    } else if (hrVal < hourValue) {
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    } else if (hrVal === hourValue) {
+      $(this).removeClass("future");
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    }
+})};
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  $("#9-time .description").val(localStorage.getItem("9-time"));
+  $("#10-time .description").val(localStorage.getItem("10-time"));
+  $("#11-time .description").val(localStorage.getItem("11-time"));
+  $("#12-time .description").val(localStorage.getItem("12-time"));
+  $("#13-time .description").val(localStorage.getItem("13-time"));
+  $("#14-time .description").val(localStorage.getItem("14-time"));
+  $("#15-time .description").val(localStorage.getItem("15-time"));
+  $("#16-time .description").val(localStorage.getItem("16-time"));
+  $("#17-time .description").val(localStorage.getItem("17-time"));
+  $("#18-time .description").val(localStorage.getItem("18-time"));
+  $("#19-time .description").val(localStorage.getItem("19-time"));
+  $("#20-time .description").val(localStorage.getItem("20-time"));
+  $("#21-time .description").val(localStorage.getItem("21-time"));
+  $("#22-time .description").val(localStorage.getItem("22-time"));
+  $("#23-time .description").val(localStorage.getItem("23-time"));
+
+  timeyWimey();
 });
